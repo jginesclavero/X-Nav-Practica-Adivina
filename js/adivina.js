d869e75b9e2c2bc392ad450bf4227d22c4208810
Jonathan Gines <jonathangines@hotmail.com>
2015-05-15 17:38:19 +0200

    incluido GEOJson capitales y su procesado

--- js/adivina.js
+++ js/adivina.js
@@ -1,13 +1,25 @@
 var typeGame;
-var TYPE_GAME1 = "capitales";
-var TYPE_GAME2 = "monumentos";
-var TYPE_GAME3 = "radio3";
-var TYPE_GAME4 = "radio4";
+var TYPE_GAME1 = "Capitales";
+var TYPE_GAME2 = "Monumentos";
+var TYPE_GAME3 = "Radio3";
+var TYPE_GAME4 = "Radio4";
 var difficult;
+var indexList = [];
+var placeList = [];
 checkURL();
 
+var map = L.map('map',{
+    center:[0,0],
+    zoom:2,
+    minzoom:2
+});
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {  // http://{s}.tile.osm.org/{z}/{x}/{y}.png
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
 function checkURL(){
-	var param = document.URL.split('#')[1];
+	var param = document.URL.split('?')[1];
 	if(param != null){
 		typeGame = param;
 		console.log(typeGame);
@@ -17,26 +29,27 @@ function checkURL(){
 	}
 }
 
-
 function typeGame() {
     if($("#radio1").prop("checked")){
     	console.log(TYPE_GAME1);
-    	location.hash = TYPE_GAME1;
     	typeGame = TYPE_GAME1;
-    	//pedir el GEOJSON correspondiente
-    	//history.pushState(null, "Adivina: Capitales", window.location.href);
+        document.title = 'Adivina - ' + TYPE_GAME1;
+    	//history.pushState(null,null, "?"+TYPE_GAME1);
     }else if($("#radio2").prop("checked")){
     	console.log(TYPE_GAME2);
-    	location.hash = TYPE_GAME2;
     	typeGame = TYPE_GAME2;
+        document.title = 'Adivina - ' + TYPE_GAME2;
+        //history.pushState(null, null, "?"+TYPE_GAME2);
     }else if($("#radio3").prop("checked")){
     	console.log(TYPE_GAME3);
-    	location.hash = TYPE_GAME3;
     	typeGame = TYPE_GAME3;
+        document.title = 'Adivina - ' + TYPE_GAME3;
+        //history.pushState(null, null, "?"+TYPE_GAME3);
     }else if($("#radio4").prop("checked")){
     	console.log(TYPE_GAME4);
-    	location.hash = TYPE_GAME4;
     	typeGame = TYPE_GAME4;
+         document.title = 'Adivina - ' + TYPE_GAME4;
+        //history.pushState(null, null, "?"+TYPE_GAME4);
     }
     showDifficult();
 }
@@ -53,6 +66,7 @@ function showTypeGame(){
 
 function showDifficult(){
 
+    $("#confWindow").show();
 	$('#configTitle').html("Seleccione la dificultad");
 	$('label[for=radio1]').html('Facil');
 	$('label[for=radio2]').html('Medio');
@@ -79,4 +93,33 @@ function selectDifficult(){
     	difficult = 4;
     	console.log("extremo");
     }
+
+    getJson(typeGame);
+}
+
+function getJson(filename){
+    var path = "/juegos/" + filename + ".json";
+    $.getJSON(path, function(data) {
+        $.each(data.features,function(posicion, place){
+            placeList.push(place);
+        });
+        startGame();
+    });
+}
+
+function startGame(){
+    console.log("COMENZAMOOS");
+    for(var i=0;i<placeList.length;i+=1){
+        var index = randIndex(placeList.length);
+        console.log(placeList[index].properties.Name);
+    }
+    console.log("Juego terminado");
+}
+
+function randIndex(max){
+    do{
+        var index = Math.floor((Math.random() * max));
+    }while($.inArray(index,indexList) != -1);
+    indexList.push(index);
+    return index;
 }
