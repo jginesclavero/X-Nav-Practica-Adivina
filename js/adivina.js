c2299fae813012ac4d51b57b9a04986e794158c5
Jonathan Gines <jonathangines@hotmail.com>
2015-05-21 11:37:46 +0200

    history api hecho

--- js/adivina.js
+++ js/adivina.js
@@ -1,14 +1,20 @@
 var typeGame;
 var TYPE_GAME1 = "Capitales";
-var TYPE_GAME2 = "Monumentos";
-var TYPE_GAME3 = "Radio3";
+var TYPE_GAME2 = "Peliculas";
+var TYPE_GAME3 = "Monumentos";
 var TYPE_GAME4 = "Radio4";
+var CONST_LAT = 113.300;
+var CONST_LNG = 111.100;
+var MAX_SCORE = 1000;
+var MARGIN_KM = 25;
+var yourScore = 0;
 var difficult;
+var presentIndex = 0;
 var indexList = [];
 var placeList = [];
 var linkList = [];
-checkURL();
-
+var handlerList = [];
+var numPhotos = 0;
 var map = L.map('map',{
     center:[0,0],
     zoom:2,
@@ -18,10 +24,66 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {  // http://{s}.tile.osm
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+function onMapClick(e) {
+    if(indexList.length <= 12){ 
+        var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+        //Limpiar otros marcadores
+        handlerOff();
+        scoreCalc(e.latlng);
+        saveHistory();
+        nextSet();
+    }else{
+        console.log("Juego terminado");
+    }
+}
+map.on('click', onMapClick);
+
+window.onpopstate = function(event) {
+    //alert("Historia recuperada");
+    indexList = event.IndexList;
+    $("#score").html(event.Score);
+    console.log("Historia recuperada");
+};
+
+checkURL();
+
+function handlerOff(){
+    for(var i=0;i<handlerList.length;i+=1){
+        clearTimeout(handlerList[i]);
+    }
+    handlerList = [];
+}
+
+
+function scoreCalc(latlngClick){
+    var latlngPhoto = placeList[presentIndex].geometry.coordinates;
+    var distance = latlngClick.distanceTo(latlngPhoto)/1000;
+    if(distance <= MARGIN_KM){
+        yourScore = MAX_SCORE / numPhotos;
+    }else{
+        yourScore = (1 - ((distance - MARGIN_KM) / distance)) * MAX_SCORE / numPhotos ;
+    }
+    var score = parseInt($("#score").html());
+    $("#score").html(Math.trunc(score + yourScore));
+    yourScore = 0;
+}
+
+
+
+function saveHistory(){
+    alert("Historia guardada");
+    var score = parseInt($("#score").html());
+    var saveObjet = {Score: score, IndexList: indexList};
+    history.pushState(saveObjet,null, "?"+typeGame);
+}
+
+
+
 
 function checkURL(){
 	var param = document.URL.split('?')[1];
 	if(param != null){
+        param = param.split('/')[0];
 		typeGame = param;
 		console.log(typeGame);
 		showDifficult();
@@ -58,10 +120,10 @@ function typeGame() {
 function showTypeGame(){
 
 	$('#configTitle').html("Seleccione el tipo de juego");
-	$('label[for=radio1]').html('Capitales');
-	$('label[for=radio2]').html('Monumentos');
-	$('label[for=radio3]').html('Radio3');
-	$('label[for=radio4]').html('Radio4');
+	$('label[for=radio1]').html(TYPE_GAME1);
+	$('label[for=radio2]').html(TYPE_GAME2);
+	$('label[for=radio3]').html(TYPE_GAME3);
+	$('label[for=radio4]').html(TYPE_GAME4);
 
 }
 
@@ -84,7 +146,6 @@ function selectDifficult(){
     	console.log("facil");
     	difficult = 1;
         TIME_DIFFICULT = 5000;
-    	//history.pushState(null, "Adivina: Capitales", window.location.href);
     }else if($("#radio2").prop("checked")){
     	console.log("medio");
     	difficult = 2;
@@ -113,13 +174,13 @@ function getJson(filename){
 }
 
 function nextSet(){
-    console.log("COMENZAMOOS");
-    //for(var i=0;i<placeList.length;i+=1){
-    var index = randIndex(placeList.length);
-    console.log(placeList[index].properties.Name);
-    getPictures(placeList[index].properties.Name);
-   // }
-    console.log("Juego terminado");
+     if(indexList.length <= 12){    // QUITAR SI QUITAMOS EL BOTON DE DEBUG
+        presentIndex = randIndex(placeList.length);
+        console.log(placeList[presentIndex].properties.Name);
+        getPictures(placeList[presentIndex].properties.Name);
+    }else{
+        console.log("juego terminado");
+    }
 }
 
 function randIndex(max){
@@ -133,6 +194,7 @@ function randIndex(max){
 function getPictures(tag){
     jQuery(document).ready(function() {
         linkList = [];
+        numPhotos = 0;
         var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?tagmode=any&id=132464580@N05&format=json&jsoncallback=?";
         $.getJSON(flickerAPI, {
             tags: tag,
@@ -153,9 +215,12 @@ function changePhoto(){
     }
 }
 
-function setHandler(index){
-    setTimeout(function(){ 
-        var img = $("<img>").attr("src",linkList[index]);
+function setHandler(photoindex){
+    var handler = setTimeout(function(){ 
+        var img = $("<img>").attr("src",linkList[photoindex]);
         $("#picturesDiv").html(img);
-    }, index*TIME_DIFFICULT,linkList,index); 
+        numPhotos+=1;
+    }, photoindex*TIME_DIFFICULT,linkList,photoindex); 
+
+    handlerList.push(handler);
 }
